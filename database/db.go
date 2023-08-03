6f95f25e4d422910d66eb9af2277784df0da5e3d
Chinmay <chaudhari.chinmay12345@gmail.com>
2023-08-03 23:12:04 +0530

    vercel

--- database/db.go
+++ database/db.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -25,7 +24,8 @@ func Collection() *mongo.Collection {
 func Init() {
 	err := godotenv.Load()
 	checkError(err)
-	connectionString := os.Getenv("DB_STRING")
+	// connectionString := os.Getenv("DB_STRING")
+	connectionString := "mongodb+srv://chinmay:chinmay@tasks.rqbaptf.mongodb.net/?retryWrites=true&w=majority"
 	println("hello world")
 	clientOption := options.Client().ApplyURI(connectionString)
 	client, err := mongo.Connect(context.TODO(), clientOption)
