a7d2fc43241942a34e548076581a7b7e034065fe
Chinmay <chaudhari.chinmay12345@gmail.com>
2023-08-04 00:19:34 +0530

    ok

--- api/index.go
+++ api/index.go
@@ -7,15 +7,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	// database.Init()
-	// registeredusers.InitializeRegisteredUsers()
-	controller.Home(w, r)
-}
-
-func Main() {
+func StartItBuddy() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", Handler).Methods("POST")
 
 	http.ListenAndServe(":4000", r)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// database.Init()
+	// registeredusers.InitializeRegisteredUsers()
+	controller.Home(w, r)
 }
